ad280f2f4482342d11f4f26d356e1c81892eea91
Takashi Sasaki <t.sasaki@bmandw.com>
2012-09-04 14:18:28 +0900

    temp

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 comfog
 ======
 Chrome Extension setting page framework.
+
+### Create Instance
+	var instance = comfog(config_object);
+
+### Render Setting Page
+Use setup method.
+
+	var instance = comfog(config_object);
+	instance.renderTo("body");	// "body" is jquery selector
+
+### About Config Object
+
+#### example
+	comfog({
+		tabs: [
+			{					//a tab config
+				label: ""
+				items: [
+					{id: "", label: "", form{}},	//a setting config
+					...
+				]
+			},
+			...
+		]
+	});
+
+/tabs				{Array}	tab settings   
+/tabs/[]/label		{String} tab caption  
+/tabs/[]/items		{Array} setting items  
+/tabs/[]/items/[]/id	{String} setting element id  
+/tabs/[]/items/[]/label	{String} settting element caption  
+/tabs/[]/items/[]/form	{Object} form config of this element  
+
+### form object
+Form object for input/output setting.
+
+**(All types)**
+
+	form: {
+		type: "fieldType", 	//field type 
+		def: "default", 	//default value.
+		size: 10			//field display size
+		listeners: {
+			"click": function		//event listener
+		}
+	}
+
+**Text**
+
+	form: {type: "text", def: "", size: 10}
+
+**NumText**
+
+	form {type: "numtext", def: 10, size: 3
+		max: 10, 	//range upper
+		min: 0		//range lower
+	}
+
+**Checkboxk**
+
+	form {type: "checkbox", def: true}
+
--- comfog.js
+++ comfog.js
@@ -1,7 +1,60 @@
 (function(exports){
-    var comfog = function(config){
-        var that = {};
+    function TabObject(tabid, tabconfig){
+        this.id = tabid;
+        this.config = $.extend(tabconfig);
+        this.items = [];
+    }
+    TabObject.prototype = {
+        getJQ: function(){
+            return $("#"+this.id);
+        },
+        addItem: function(item){
+            this.items.push(item);
+        }
+        
+    };
 
+    function ItemObject(itemconfig){
+        this.id = itemconfig.id;
+        this.config = $.extend(itemconfig);
+    }
+    ItemObject.prototype = {
+        getJQ: function(){
+            return $("#"+this.id);
+        },
+        getValue: function(){
+            return $("#"+this.formid).val();
+        },
+        createForm: function(){
+            var fconf = this.config.form;
+            var form;
+            if (fconf.type == "text"){
+                form = $('<input type="text"/>');
+            } else if (fconf.type == "button"){
+                form = $('<button></button>');
+            } else {
+                return;
+            }
+            if (fconf.size){
+                form.attr("size", fconf.size);
+            }
+            if (fconf.label){
+                form.text(fconf.label);
+            }
+            if (fconf.listeners){
+                $.each(fconf.listeners, function(key, value){
+                    form.on(key, value);
+                });
+            }
+            return form;
+        }
+
+    }
+
+    var comfog = function(config){
+        var that = {
+            tabs: {},
+        };
 
         function setup(selector){
             var cont = $(selector);
@@ -15,10 +68,34 @@
             $.each(config.tabs, function(idx, tabconfig){
                 var tabid = tabconfig.id || ""+idx;
                 tabid = "tabs-"+tabid;
+                //tab index
                 $('<li/>').append(
                     $('<a href="#'+tabid+'"/>').text(tabconfig.label)
                 ).appendTo("#tabs ul");
+
+                //tab body
                 $('<div id="'+tabid+'"/>').appendTo("#tabs-inner");
+
+                var tab =new TabObject(tabid, tabconfig);
+                that.tabs[tabid] = tab;
+                var tabJQ = tab.getJQ();
+
+                if (!tabconfig.items){
+                    return;
+                }
+                //tab items
+                $.each(tabconfig.items, function(idx2, itemconfig){
+                    var itemObj =new ItemObject(itemconfig); 
+                    tab.addItem(itemObj);
+
+                    var cont = $('<div id="'+itemconfig.id+'_cont"/>').append(
+                        $('<label for="'+itemconfig.id+'">').text(itemconfig.label)
+                    );
+                    if (itemconfig.form){
+                        cont.append(itemObj.createForm());
+                    }
+                    tabJQ.append(cont);
+                });
             });
             $("#tabs").tabs();
 
